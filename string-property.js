d167134a93bb8df8a8a9c090ae9e5e30c51bd066
zoynulabedin <trustcodernet@gmail.com>
2019-03-19 18:39:18 +0600

    daily update

---
+++ string-property.js
@@ -0,0 +1,19 @@
+// upper case
+// var st = "Hello world";
+// var up = st.toUpperCase();
+// console.log(up);
+
+// lower case with let variable
+
+// let st = "Hello world";
+// let lc = st.toLowerCase();
+// console.log(lc);
+// let st = "go to prothomalo";
+// let lin = st.link("www.prthomalo.com");
+// console.log(lin);
+
+// repeat text
+
+// let st = "Hello world";
+// let rep = st.repeat(50);
+// console.log(rep);
